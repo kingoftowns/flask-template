5a7b7fa8b93f02cc45767e64d54526d15362c25f
Michael Reynaga <michael@blacktoaster.com>
2025-08-25 15:01:46 -0700

    return task id, add /api/task/<task_id> endpoint, devcontainers

--- src/flask-app/api/routes.py
+++ src/flask-app/api/routes.py
@@ -1,6 +1,7 @@
 """Defines the routes for the API"""
 
-from flask import Flask, request, abort
+from flask import Flask, jsonify
+from celery.result import AsyncResult
 
 from ..worker import tasks
 from .. import config as cfg
@@ -15,5 +16,17 @@ def api_version():
 
 @FLASK.route("/api/reverse/<str_to_reverse>")
 def reverse_string(str_to_reverse):
-    tasks.reverse.delay(str_to_reverse)
-    return "Task added!"
+    task = tasks.reverse.delay(str_to_reverse)
+    return jsonify({"task_id": task.id})
+
+@FLASK.route("/api/task/<task_id>")
+def get_task_status(task_id):
+    task = AsyncResult(task_id, app=tasks.CELERY)
+    
+    response = {
+        'task_id': task_id,
+        'state': task.state,
+        'message': task.result if task.state == 'SUCCESS' else str(task.info) if task.state == 'FAILURE' else 'Task is processing'
+    }
+    
+    return jsonify(response)
